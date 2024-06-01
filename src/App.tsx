87d66b139d4255d517416777eac3f6a1d855778f
Francisco Soto <panchon.ues@gmail.com>
2024-06-01 13:08:17 -0700

    react navigation install

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>App.tsx</Text>
-    </View>
+    </NavigationContainer>
   )
 }
 

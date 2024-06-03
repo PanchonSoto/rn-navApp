34b430eddf8f043046096aebebfaaf80d7d7ff49
Francisco Soto <panchon.ues@gmail.com>
2024-06-03 12:43:09 -0700

    popToTop/back to root

--- src/presentation/screens/settings/SettingScreen.tsx
+++ src/presentation/screens/settings/SettingScreen.tsx
@@ -1,12 +1,31 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { StackActions, useNavigation } from '@react-navigation/native';
+
+import { globalStyles } from '../../themes/theme';
+import PrimaryButton from '../../components/shared/PrimaryButton';
+
+
+
 
 const SettingScreen = () => {
+
+  const navigator = useNavigation();
+
   return (
-    <View>
+    <View style={globalStyles.container}>
       <Text>SettingScreen</Text>
+      <PrimaryButton 
+        label='Back'
+        onPress={()=> navigator.goBack()}
+      />
+
+      <PrimaryButton 
+        label='Back home'
+        onPress={()=> navigator.dispatch(StackActions.popToTop())}
+      />
     </View>
   )
 }
 
-export default SettingScreen
+export default SettingScreen;

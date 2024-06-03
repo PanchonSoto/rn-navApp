2a8397b3ba12ae2252ee5c5892be77ca82858ac9
Francisco Soto <panchon.ues@gmail.com>
2024-06-03 13:36:14 -0700

    useSafeAreaInsets

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,29 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+
+import PrimaryButton from '../../components/shared/PrimaryButton';
+
+
+
 
 const ProfileScreen = () => {
+
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{
+      flex: 1,
+      paddingHorizontal: 20,
+      marginTop: top
+    }}>
+      <Text style={{marginBottom:10}}>ProfileScreen</Text>
+      <PrimaryButton 
+        onPress={()=>navigation.dispatch(DrawerActions.toggleDrawer)}
+        label='Open menu'
+      />
     </View>
   )
 }
